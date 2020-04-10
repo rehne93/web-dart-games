9c9ef8775fa6959d622d26f30ca3fa7e98ddd6ae
=René Bärnreuther <rene.baernreuther@web.de>
2020-04-10 11:44:04 +0200

    Aktuellen Stand gemerged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import {HalveitModule} from './halveit/halveit.module';
 import {AppRoutingModule} from './app-routing.module';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MulliganModule} from './mulligan/mulligan.module';
-import {X01Module} from './x01/x01.module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { X01Module } from './x01/x01.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
